aecb1e52f96a374b50b5ed32a613458e45789fc1
pyhumph <humphrey.david.dev@gmail.com>
2024-03-17 10:36:15 +0300

    Add Python script to quiz

--- quiz.py
+++ quiz.py
@@ -4,3 +4,9 @@ playing = input("Do you want to play? ")
 
 if playing.lower() == 'yes':
     print("Okay! Let's play :)")
+
+answer = input("What does CPU stands for? ")
+if answer.lower() == "central processing unit":
+    print("Correct!")
+else:
+    print("Incorrect!")
