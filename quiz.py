457ffca4082a6c18231f5ebff17520d776ca1360
pyhumph <humphrey.david.dev@gmail.com>
2024-03-17 10:57:36 +0300

    add testing knowledge questions

--- quiz.py
+++ quiz.py
@@ -10,3 +10,58 @@ if answer.lower() == "central processing unit":
     print("Correct!")
 else:
     print("Incorrect!")
+
+answer = input("What does GPU stands for? ")
+if answer.lower() == "graphics processing unit":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("What does RAM stands for? ")
+if answer.lower() == "random access memory":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+
+answer = input("What does PSU stands for? ")
+if answer.lower() == "power supply unit":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("What does HDD stands for? ")
+if answer.lower() == "hard disk drive":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("What does SSD stands for? ")
+if answer.lower() == "solid state drive":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("What does BIOS stands for? ")
+if answer.lower() == "basic input/output system" or "basic input output system":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("What does VGA stands for? ")
+if answer.lower() == "video graphics array":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("What does NIC stands for? ")
+if answer.lower() == "network interface card":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("What does CMOS stands for? ")
+if answer.lower() == "complementary mental-oxide semiconductor" or "complementary mental oxide semiconductor":
+    print("Correct!")
+else:
+    print("Incorrect!")
